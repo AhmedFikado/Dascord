575f1d2eed8d68252e12524c45143f2221880332
EthanLedouble <ethan.ledouble@epitech.eu>
2026-03-18 14:26:15 +0100

    [FIX] traduction update

--- client/src/components/server/member-list.tsx
+++ client/src/components/server/member-list.tsx
@@ -33,16 +33,16 @@ export default function MemberList({ searchQuery = '', isRole = false, listMembe
             ) : (
                 <>
                     <div className="ml-6">
-                        <h4 className="py-2 text-[10px] font-semibold text-gray-50 uppercase">Owner</h4>
+                        <h4 className="py-2 text-[10px] font-semibold text-gray-50 uppercase">{t('Member_list.Owner')}</h4>
                         {usersOwner.map((member) => (
                             <MemberItem key={member.user.id} member={member} serverId={serverId} />
                         ))}
                             
-                        <h4 className="py-2 mt-2 text-[10px] font-semibold text-gray-50 uppercase">Admin</h4>
+                        <h4 className="py-2 mt-2 text-[10px] font-semibold text-gray-50 uppercase">{t('Member_list.Admin')}</h4>
                         {usersAdmin.map((member) => (
                             <MemberItem key={member.user.id} member={member} serverId={serverId} />
                         ))}
-                            <h4 className="py-2 mt-2 text-[10px] font-semibold text-gray-50 uppercase">Member</h4>
+                            <h4 className="py-2 mt-2 text-[10px] font-semibold text-gray-50 uppercase">{t('Member_list.Member')}</h4>
                         {usersMember.map((member) => (
                             <MemberItem key={member.user.id} member={member} serverId={serverId} />
                         ))}
